ca2e1f81ba871e85e94a16f7557b20af3c7be27b
Zambu <jose@aurestic.es>
2020-10-05 11:36:26 +0200

    [FIX] License value

--- fetchmail_attach_from_folder_bydate/__openerp__.py
+++ fetchmail_attach_from_folder_bydate/__openerp__.py
@@ -5,7 +5,6 @@
     "version": "8.0.1.0",
     "category": "Discuss",
     "author": "Aures TIC",
-    "license": 'Tools',
     "depends": [
         'fetchmail',
         'fetchmail_attach_from_folder',
