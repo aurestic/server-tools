03d670fc2d28b220918f3a0d390c20103335a14a
Jose Zambudio Bernabeu <jose@aurestic.es>
2022-07-13 13:24:53 +0200

    [MIG] fetchmail_attach_from_folder from 11.0

---
+++ fetchmail_attach_from_folder/match_algorithm/base.py
@@ -0,0 +1,23 @@
+# Copyright - 2013-2018 Therp BV <https://therp.nl>.
+# Copyright - 2020 Aures Tic Consultors S.L <https://www.aurestic.es>.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+class Base(object):
+    name = None  # Name shown to the user
+
+    # Fields on fetchmail_server folder required for this algorithm
+    required_fields = []
+
+    # Fields on fetchmail_server folder readonly for this algorithm
+    readonly_fields = []
+
+    def search_matches(self, folder, mail_message):
+        """Returns recordset found for model with mail_message."""
+        return []
+
+    def handle_match(
+        self, connection, match_object, folder, mail_message, mail_message_org, msgid
+    ):
+        """Do whatever it takes to handle a match"""
+        folder.attach_mail(match_object, mail_message)
