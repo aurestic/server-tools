423d915b4a8fd775110b4ced0c3c6a1f7dbbf0a8
almumu <almumu@users.noreply.github.com>
2023-03-24 09:27:40 +0100

    módulo fetchmail_attach_from_folder para la 15 (#10)
    * módulo fetchmail_attach_from_folder para la 15
    
    * adaptado traducciones a v15

---
+++ fetchmail_attach_from_folder/__init__.py
@@ -0,0 +1,7 @@
+# Copyright - 2013-2018 Therp BV <https://therp.nl>.
+# Copyright - 2020 Aures Tic Consultors S.L <https://www.aurestic.es>.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import match_algorithm
+from . import models
+from . import wizard
