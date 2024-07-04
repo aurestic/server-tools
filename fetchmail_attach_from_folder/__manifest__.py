4cfbb9e74a84458d2ab7076cbd1b51b61226aa1a
Jose Zambudio <jose@aurestic.es>
2024-07-04 07:20:20 +0200

    [ADD] fetchmail_attach_from_folder

---
+++ fetchmail_attach_from_folder/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright - 2013-2018 Therp BV <https://therp.nl>.
+# Copyright - 2022 Aures Tic Consultors S.L <https://www.aurestic.es>.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Email gateway - folders",
+    "summary": "Attach mails in an IMAP folder to existing objects",
+    "version": "14.0.1.0.0",
+    "author": "Aures Tic, Therp BV,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/server-tools",
+    "license": "AGPL-3",
+    "category": "Tools",
+    "depends": ["fetchmail"],
+    "data": [
+        "views/fetchmail_server.xml",
+        "wizard/attach_mail_manually.xml",
+        "security/ir.model.access.csv",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
